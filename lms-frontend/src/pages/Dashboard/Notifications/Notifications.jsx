53625402a3f39589d7110bc964b6e5d60257543b
YASHIKA MAHAWAR <honeymahawar18050309@gmail.com>
2026-07-20 22:36:38 +0530

    fix: complete fix final part

--- lms-frontend/src/pages/Dashboard/Notifications/Notifications.jsx
+++ lms-frontend/src/pages/Dashboard/Notifications/Notifications.jsx
@@ -12,7 +12,7 @@ const Notifications = () => {
         const user = JSON.parse(localStorage.getItem("user"));
 
         const res = await API.get(
-  `/notifications/${user.id}`
+  `/api/notifications/${user.id}`
 );
 setNotifications(res.data.notifications || []);
       } catch (err) {
